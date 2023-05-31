c85f24a65a0e62d8ae74f264222f524be72f90df
abdel-ios <abdeldeveloper01@gmail.com>
2023-05-31 17:14:55 +0200

    Setting up LaunchScreen UI

---
+++ Hubble/LaunchScreen.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Hubble
+//
+//  Created by Abdel Baali on 31/05/23.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    
+    @State private var isActive: Bool = false
+    
+    var body: some View {
+        
+        if isActive {
+            WelcomeScreen()
+        }else {
+            ZStack{
+                Color.neutral_900.ignoresSafeArea()
+                
+                VStack{
+                    
+                    Spacer()
+                    
+                    Image("exp-logo-purple")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 190, height: 190)
+                        .offset(x: -5)
+                        .padding(.top, 30)
+                    
+                    Spacer()
+                    
+                    Text("Hubble Inc.")
+                        .foregroundColor(.primary_500)
+                        .font(.system(size: 15, weight: .regular))
+                        .padding(.bottom, 30)
+                }
+            }
+            .preferredColorScheme(.dark)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreen()
+    }
+}
---
+++ Hubble/WelcomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeScreen.swift
+//  Hubble
+//
+//  Created by Abdel Baali on 31/05/23.
+//
+
+import SwiftUI
+
+struct WelcomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeScreen()
+    }
+}
