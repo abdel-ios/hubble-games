cbddfc48284f10a14d479e0e752f591298151935
abdel-ios <abdeldeveloper01@gmail.com>
2023-05-31 16:50:02 +0200

    Initial Commit

---
+++ Hubble/Helpers/ColorsHelper.swift
@@ -0,0 +1,88 @@
+//
+//  ColorsHelper.swift
+//  Hubble
+//
+//  Created by Abdel Baali on 31/05/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    // MARK: Neutral Colors
+    static let neutral_10 = Color("neutral-10")
+    static let neutral_50 = Color("neutral-50")
+    static let neutral_100 = Color("neutral-100")
+    static let neutral_200 = Color("neutral-200")
+    static let neutral_300 = Color("neutral-300")
+    static let neutral_400 = Color("neutral-400")
+    static let neutral_500 = Color("neutral-500")
+    static let neutral_600 = Color("neutral-600")
+    static let neutral_700 = Color("neutral-700")
+    static let neutral_800 = Color("neutral-800")
+    static let neutral_900 = Color("neutral-900")
+    
+    //MARK: Primary Colors
+    static let primary_50 = Color("primary-50")
+    static let primary_100 = Color("primary-100")
+    static let primary_200 = Color("primary-200")
+    static let primary_300 = Color("primary-300")
+    static let primary_400 = Color("primary-400")
+    static let primary_500 = Color("primary-500")
+    static let primary_600 = Color("primary-600")
+    static let primary_700 = Color("primary-700")
+    static let primary_800 = Color("primary-800")
+    static let primary_900 = Color("primary-900")
+    
+    //MARK: Secondary Colors
+    static let secondary_50 = Color("secondary_50")
+    static let secondary_100 = Color("secondary_100")
+    static let secondary_200 = Color("secondary_200")
+    static let secondary_300 = Color("secondary_300")
+    static let secondary_400 = Color("secondary_400")
+    static let secondary_500 = Color("secondary_500")
+    static let secondary_600 = Color("secondary_600")
+    static let secondary_700 = Color("secondary_700")
+    static let secondary_800 = Color("secondary_800")
+    static let secondary_900 = Color("secondary_900")
+    
+    //MARK: Others
+    
+    // Blues
+    static let blue_100 = Color("blue-100")
+    static let blue_300 = Color("blue-300")
+    static let blue_500 = Color("blue-500")
+    static let blue_700 = Color("blue-700")
+    static let blue_900 = Color("blue-900")
+    
+    //Greens
+    static let green_100 = Color("green-100")
+    static let green_300 = Color("green-300")
+    static let green_500 = Color("green-500")
+    static let green_700 = Color("green-700")
+    static let green_900 = Color("green-900")
+    
+    //Reds
+    static let red_100 = Color("red-100")
+    static let red_300 = Color("red-300")
+    static let red_500 = Color("red-500")
+    static let red_700 = Color("red-700")
+    static let red_900 = Color("red-900")
+    
+    //Yellows
+    static let yellow_100 = Color("yellow-100")
+    static let yellow_300 = Color("yellow-300")
+    static let yellow_500 = Color("yellow-500")
+    static let yellow_700 = Color("yellow-700")
+    static let yellow_900 = Color("yellow-900")
+    
+    //Mangos
+    static let mango_100 = Color("mango-100")
+    static let mango_300 = Color("mango-300")
+    static let mango_500 = Color("mango-500")
+    static let mango_700 = Color("mango-700")
+    static let mango_900 = Color("mango-900")
+    
+    
+}
---
+++ Hubble/HubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HubbleApp.swift
+//  Hubble
+//
+//  Created by Abdel Baali on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
